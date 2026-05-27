6010d04f41f8c3544ca07c92a3344020b19ae4f6
vrushitarnw2266 <vrushita2366@gmail.com>
2026-05-27 15:21:44 +0530

    feat(new):Grade_Generator

--- Grade Generator/index.cpp
+++ Grade Generator/index.cpp
@@ -1,34 +1,74 @@
 #include <iostream>
 using namespace std;
-int main()
-{
+
+int main() {
+
     int marks;
+    char grade;
 
     cout << "Enter your marks: ";
     cin >> marks;
 
-    if (marks >= 90 && marks <= 100)
-    {
-        cout << "GRADE: A+";
+    if (marks >= 90 && marks <= 100) {
+        grade = 'A';
     }
-    else if (marks >= 80)
-    {
-        cout << "GRADE:A";
+    else if (marks >= 80) {
+        grade = 'B';
     }
-    else if (marks >= 70)
-    {
-        cout << "GRADE:B";
+    else if (marks >= 70) {
+        grade = 'C';
     }
-    else if (marks >= 60)
-    {
-        cout << "GRADE:C";
+    else if (marks >= 60) {
+        grade = 'D';
     }
-    else if (marks >= 50)
-    {
-        cout << "GRADE:D";
+    else if (marks >= 35) {
+        grade = 'E';
     }
-    else
-    {
-        cout << "GRADE:F";
+    else {
+        grade = 'F';
     }
+
+  
+    cout << "Your Grade is: " << grade << endl;
+
+ 
+    switch(grade) {
+
+        case 'A':
+            cout << "Excellent Performance";
+            break;
+
+        case 'B':
+            cout << "Very Good";
+            break;
+
+        case 'C':
+            cout << "Good";
+            break;
+
+        case 'D':
+            cout << "Average";
+            break;
+
+        case 'E':
+            cout << "Need Improvement";
+            break;
+
+        case 'F':
+            cout << "Fail";
+            break;
+
+        default:
+            cout << "Invalid Marks";
+    }
+
+  
+    if (marks >= 35) {
+        cout << "\nEligible for Next Level";
+    }
+    else {
+        cout << "\nNot Eligible for Next Level";
+    }
+
+    return 0;
 }
